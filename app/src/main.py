8085b9ce06cd7af32a4c47b78a92dd6c38e0e2db
CosmicOriginDev <154019149+CosmicOriginDev@users.noreply.github.com>
2026-02-21 15:36:04 -0600

    added flask

--- app/src/main.py
+++ app/src/main.py
@@ -1,2 +1,11 @@
-print("Hello, World")
+from flask import Flask
+from flask_socketio import SocketIO, emit
+app = Flask(__name__)
+socketio = SocketIO(app)
 
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True)
