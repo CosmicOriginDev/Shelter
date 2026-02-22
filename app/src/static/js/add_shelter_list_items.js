1e9bec1e3088021594ceacb9e70bab1e14cd54fa
Ed0827 <seyun.jeong@slu.edu>
2026-02-21 21:53:09 -0600

    it works

--- app/src/static/js/add_shelter_list_items.js
+++ app/src/static/js/add_shelter_list_items.js
@@ -8,12 +8,19 @@ const template = document.getElementById("shelter-list-item-template");
 socket.emit('send_shelters')
 
 socket.on("shelter_data", (data) => {
+  console.log("DATA:", data);
+  console.log("TYPE:", typeof data);
+  console.log("IS ARRAY:", Array.isArray(data));
+});
+
+socket.on("shelter_data", (data) => {
   console.log("Received shelters:", data);
-  data.forEach(shelter => {
+  shelters = data.data
+  shelters.forEach(shelter => {
     const clone = template.content.cloneNode(true);
   
     clone.querySelector(".name").textContent = shelter.name;
-    clone.querySelector(".capacity").textContent = `Beds available: ${shelter.current_population} / ${shelter.capacity}`;
+    clone.querySelector(".capacity").textContent = `Beds available: ${shelter.current_population} / ${shelter.max_people}`;
     clone.querySelector(".map-link").href = shelter.map_link;
   
     container.appendChild(clone);
