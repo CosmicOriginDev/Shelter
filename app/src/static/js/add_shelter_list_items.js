86266205df9e032732690dccdb134f2bf28c9da1
CosmicOriginDev <154019149+CosmicOriginDev@users.noreply.github.com>
2026-02-21 20:29:48 -0600

    dfgdf

--- app/src/static/js/add_shelter_list_items.js
+++ app/src/static/js/add_shelter_list_items.js
@@ -1,19 +1,28 @@
+const socket = io();
+var shelters = [];
 
-const shelters = [
-    { name: "Shelter A", capacity: 120, map_link: "https://maps.google.com/?q=A" },
-    { name: "Shelter B", capacity: 80,  map_link: "https://maps.google.com/?q=B" },
-    { name: "Shelter C", capacity: 200, map_link: "https://maps.google.com/?q=C" }
-  ];
+socket.on("shelters", (data) => {
+  console.log("Received shelters:", data);
+  var new_data = JSON.parse(data);
+  shelters.push(new_data);
+});
+
+
 
 const container = document.getElementById("shelter-list");
 const template = document.getElementById("shelter-list-item-template");
 
-shelters.forEach(shelter => {
-  const clone = template.content.cloneNode(true);
 
-  clone.querySelector(".name").textContent = shelter.name;
-  clone.querySelector(".capacity").textContent = `Capacity: ${shelter.capacity}`;
-  clone.querySelector(".map-link").href = shelter.map_link;
-
-  container.appendChild(clone);
+socket.on("shelters_ready", (data) => {
+  shelters.forEach(shelter => {
+    const clone = template.content.cloneNode(true);
+  
+    clone.querySelector(".name").textContent = shelter.name;
+    clone.querySelector(".capacity").textContent = `Capacity: ${shelter.capacity}`;
+    clone.querySelector(".map-link").href = shelter.map_link;
+  
+    container.appendChild(clone);
+  });
 });
+
+
